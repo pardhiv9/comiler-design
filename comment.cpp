0f9641cf12d8042992f3292e8489c6288e2ddb42
pardhiv9 <113880142+pardhiv9@users.noreply.github.com>
2022-09-19 11:39:05 +0530

    Add files via upload

---
+++ comment.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+char com [30];
+int i=2,a=0;
+
+printf("\n Enter Text : ");
+
+gets(com);
+if(com[0]=='/')
+{
+if(com[1]=='/')
+
+printf("\n It is a single Comment.");
+
+else if(com [1]=='*')
+{
+for(i=2;i<=30;i++)
+{
+if(com[i]=='*'&&com[i+1]=='/')
+{
+printf("\n It is a multiline Comment.");
+a=1;
+break;
+}
+else continue;
+}
+if(a==0)
+
+printf("\n It is Not a Comment.");
+}
+else
+
+printf("\n It is Not a Comment.");
+
+}
+else
+
+printf("\n It is Not a Comment.");
+
+}
